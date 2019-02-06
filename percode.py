0642f908864c9f19f60b92c45a41c3971979926c
mttaat <47231067+mttaat@users.noreply.github.com>
2019-02-05 16:36:08 -0800

    Added percode.py - string percent en/decoder

---
+++ percode.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+#
+# Copyright (c) 2019 mttaat <mttaat@protonmail.com>. All Rights Reserved.
+# This file licensed under the GPLv3
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# percode v0.1
+# percent encode/decode arbitrary strings
+#
+# usage: $ python percode.py -e '<STRING>'
+# usage: $ python percode.py -d %3c%53%54%52%49%4e%47%3e
+#
+# # # # # # # # 
+#
+# @mttaat
+# https://mttaat.net
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+
+
+import sys
+import re
+
+if sys.argv[1] == "-e":
+ un = sys.argv[2]
+ un = str(un)
+ en = ""
+ for ch in un:
+  ch = str(ch)
+  en += "%" + str(ch.encode("hex"))
+ print(en)
+elif sys.argv[1] == "-d":
+ en = sys.argv[2]
+ en = en.replace("%", "")
+ un = str(en.decode("hex"))
+ print(un)
+else:
+ print("invalid input")
